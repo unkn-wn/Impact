dfe8bb8681025fca81016cd88e570126e102b4d4
Leon <43097991+unkn-wn@users.noreply.github.com>
2020-02-22 18:14:43 -0800

    Delete config.php

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-   define('DB_SERVER', 'localhost');
-   define('DB_USERNAME', 'root');
-   define('DB_PASSWORD', 'fightfiinfo888');
-   define('DB_DATABASE', 'impact');
-   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-?>
-
